6dec3d577e3bf6e27cbf3b71eeab78698bb6b734
Hinh1009 <dinhhinh10091998@gmail.com>
2022-07-13 18:47:40 +0700

    fix conflict

--- src/components/portfolio/WalletAddress.js
+++ src/components/portfolio/WalletAddress.js
@@ -1,35 +1,42 @@
 import React, { useState, useEffect } from 'react'
+import { DeleteOutlined, EditOutlined } from '@ant-design/icons'
 import { Button, Col, Row, Collapse, Tabs, Divider, Input } from 'antd'
-import { } from '@ant-design/icons'
 import './styles.scss'
-import { PORTFOLIO_CONNECT } from '../../constants/TypeConstants'
 import { useDispatch, useSelector } from 'react-redux'
-import { getAllAddress } from '../../redux/addressSlice'
+import { getAllConnection, deleteConnection } from '../../redux/addressSlice'
 import ModalContent from '../modal/connect-portfolio'
-
+import ModalEdit from '../modal/modal-edit'
+import { SUCCESS_DELETE_CONNECTION } from '../../constants/StatusMessageConstants'
 const { TabPane } = Tabs
-// const { Link } = Typography
 const WalletAddress = () => {
   const onSearch = (value) => console.log(value)
   const [isModalVisible, setIsModalVisible] = useState(false)
-  const [type, setType] = useState(PORTFOLIO_CONNECT)
+  const [isModalEdit, setIsModalEdit] = useState(false)
+  const [dataEdit, setDataEdit] = useState(false)
   const dispatch = useDispatch()
-
+  const { list_connection, status } = useSelector(state => state.connections)
   const { Search } = Input
-
-  const { list_address } = useSelector(state => state.address)
-
-  // console.log(list_address)
   const handleConnectPortfolio = () => {
     setIsModalVisible(true)
   }
 
+  console.log(list_connection)
   useEffect(() => {
-    dispatch(getAllAddress())
-  }, [dispatch])
+    dispatch(getAllConnection())
+  }, [dispatch, isModalEdit, status === SUCCESS_DELETE_CONNECTION])
 
   const handleTabClick = (e) => {
     console.log(e)
+  }
+
+  const handleDeleteAddress = (id) => {
+    console.log(id)
+    dispatch(deleteConnection(id))
+  }
+
+  const handleEditAddress = (item) => {
+    setIsModalEdit(true)
+    setDataEdit(item)
   }
 
   return (
@@ -47,18 +54,34 @@ const WalletAddress = () => {
           <Collapse className='panel' ghost defaultActiveKey={['1']}>
             <TabPane tab='All Assets' key='all' />
             <Tabs tabPosition='left' onTabClick={handleTabClick}>
-              {list_address && list_address.map((item) => (
-                <TabPane tab={item.connectionName} key={item.id} />
+              {list_connection && list_connection.map((item) => (
+                <TabPane
+                  tab={
+                    <div>
+                      {item.connectionName}
+                      <DeleteOutlined onClick={() => handleDeleteAddress(item.id)}/>
+                      <EditOutlined onClick={() => handleEditAddress(item)}/>
+                    </div>
+                  }
+                  key={item.id}
+                />
               ))}
+              {/* <TabPane tab='Tab 1' key='1' />
+              <TabPane tab='Tab 2' key='2' />
+              <TabPane tab='Tab 3' key='3' /> */}
             </Tabs>
+            {/* </Panel> */}
           </Collapse>
         </Col>
       </Row>
       <ModalContent
         isModalVisible={isModalVisible}
         setIsModalVisible={setIsModalVisible}
-        type={type}
-        setType={setType}
+      />
+      <ModalEdit
+        isModalEdit={isModalEdit}
+        setIsModalEdit={setIsModalEdit}
+        dataEdit={dataEdit}
       />
     </div>
   )
