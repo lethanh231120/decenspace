165a931480489522dc2871f5da86d36dafd126e7
lê thành <lethanh231120@gmail.com>
2022-07-20 17:09:35 +0700

    fix commit 2

--- src/components/auth/confirm-email/index.js
+++ src/components/auth/confirm-email/index.js
@@ -1,16 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import queryString from 'query-string'
 export const ConfirmEmail = () => {
   const [message, setMessage] = useState()
   useEffect(() => {
     const queryParams = new URLSearchParams(window.location.search)
-    const queryParams2 = queryString.parse(window.location.search)
-    console.log('queryParams2', queryParams2)
     const uuid = queryParams.get('uuid')
     const token = queryParams.get('token')
-    console.log('uuid', uuid)
-    console.log('token', token)
     const authoriation = async() => {
       const instance = axios.create({
         baseURL: '/accountService'
