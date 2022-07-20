7698d1a17ac094fed5a8d922f30f75ffbea8fa81
lê thành <lethanh231120@gmail.com>
2022-07-20 16:13:25 +0700

    fix netlify not found 4

--- src/components/auth/confirm-email/index.js
+++ src/components/auth/confirm-email/index.js
@@ -1,7 +1,24 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 export const ConfirmEmail = () => {
+  const [message, setMessage] = useState()
+  useEffect(() => {
+    const queryParams = new URLSearchParams(window.location.search)
+    const uuid = queryParams.get('uuid')
+    const token = queryParams.get('token')
+    const authoriation = async() => {
+      try {
+        axios.defaults.headers.common['Authorization'] = token
+        await axios.get(`accounts/confirm-email/uuid=${uuid}`)
+        setMessage('Verify email successfully!')
+      } catch (error) {
+        console.log(error?.response?.data?.message)
+      }
+    }
+    authoriation()
+    console.log(uuid, token)
+  }, [])
   return (
-    <div>ConfirmEmail</div>
+    <div style={{ padding: '50px 0' }}>{message && message}</div>
   )
 }
