94c373381ca79bb77cf4645ab707a687d94b5c06
lê thành <lethanh231120@gmail.com>
2022-07-21 14:07:11 +0700

    fix conflict main

--- next.config.js
+++ next.config.js
@@ -5,13 +5,7 @@ module.exports = {
         {
           source: '/addressService/:path*',
           destination: `http://139.180.147.199:8082/:path*`,
-          permanent: false,
-          headers: [
-            { key: 'Access-Control-Allow-Credentials', value: 'true' },
-            { key: 'Access-Control-Allow-Origin', value: '*' },
-            { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
-            { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' }
-          ]
+          permanent: false
         },
         {
           source: '/accountService/:path*',
@@ -28,13 +22,7 @@ module.exports = {
         {
           source: '/coinPriceService/:path*',
           destination: `http://139.180.147.199:8081/:path*`,
-          permanent: false,
-          headers: [
-            { key: 'Access-Control-Allow-Credentials', value: 'true' },
-            { key: 'Access-Control-Allow-Origin', value: '*' },
-            { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
-            { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' }
-          ]
+          permanent: false
         }
       ]
     }
