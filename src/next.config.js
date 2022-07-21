cf18fae84454ca0f455b693c96e3f9cdfe7b1710
lê thành <lethanh231120@gmail.com>
2022-07-21 13:39:55 +0700

    fig

--- src/next.config.js
+++ src/next.config.js
@@ -5,17 +5,35 @@ module.exports = {
         {
           source: '/addressService/:path*',
           destination: `http://139.180.147.199:8082/:path*`,
-          permanent: false
+          permanent: false,
+          headers: [
+            { key: 'Access-Control-Allow-Credentials', value: 'true' },
+            { key: 'Access-Control-Allow-Origin', value: '*' },
+            { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+            { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' }
+          ]
         },
         {
           source: '/accountService/:path*',
           destination: `http://139.180.147.199:8080/:path*`,
-          permanent: false
+          permanent: false,
+          headers: [
+            { key: 'Access-Control-Allow-Credentials', value: 'true' },
+            { key: 'Access-Control-Allow-Origin', value: '*' },
+            { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+            { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' }
+          ]
         },
         {
           source: '/coinPriceService/:path*',
           destination: `http://139.180.147.199:8081/:path*`,
-          permanent: false
+          permanent: false,
+          headers: [
+            { key: 'Access-Control-Allow-Credentials', value: 'true' },
+            { key: 'Access-Control-Allow-Origin', value: '*' },
+            { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+            { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' }
+          ]
         }
       ]
     }
