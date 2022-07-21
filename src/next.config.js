7b95a57138012df8f07964a97133f2f6ffddc597
lê thành <lethanh231120@gmail.com>
2022-07-21 11:04:49 +0700

    add next.json vercel 1

--- src/next.config.js
+++ src/next.config.js
@@ -1,11 +1,23 @@
 module.exports = {
   experimental: {
     async redirects() {
-      return [{
-        source: '/addressService/:path*',
-        destination: `http://139.180.147.199:8082/:path*`,
-        permanent: false
-      }]
+      return [
+        {
+          source: '/addressService/:path*',
+          destination: `http://139.180.147.199:8082/:path*`,
+          permanent: false
+        },
+        {
+          source: '/accountService/:path*',
+          destination: `http://139.180.147.199:8080/:path*`,
+          permanent: false
+        },
+        {
+          source: '/coinPriceService/:path*',
+          destination: `http://139.180.147.199:8081/:path*`,
+          permanent: false
+        }
+      ]
     }
   }
 }
