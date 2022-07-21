adcd89681978eaa69ee53ccadfe7ce11422466a5
lê thành <lethanh231120@gmail.com>
2022-07-21 14:09:47 +0700

    fix nexxt.jso

--- next.config.js
+++ next.config.js
@@ -11,13 +11,7 @@ module.exports = {
           source: '/accountService/:path*',
           destination: `https://e811-118-70-117-216.ap.ngrok.io/:path*`,
           // destination: `http://139.180.147.199:8080/:path*`,
-          permanent: false,
-          headers: [
-            { key: 'Access-Control-Allow-Credentials', value: 'true' },
-            { key: 'Access-Control-Allow-Origin', value: '*' },
-            { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
-            { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' }
-          ]
+          permanent: false
         },
         {
           source: '/coinPriceService/:path*',
