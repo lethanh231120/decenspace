a5f66e3a32a5a6b9fe1065f9ac27eeaee4b850a3
lê thành <lethanh231120@gmail.com>
2022-07-27 22:16:59 +0700

    fix webpack

--- src/components/platforms/TrustWallet.js
+++ src/components/platforms/TrustWallet.js
@@ -3,7 +3,7 @@ import { Typography, Form, Button, Tabs, Input } from 'antd'
 import { PlatformHeader } from './form-input/PlatformHeader'
 import { ConnectionName } from './form-input/ConnectionName'
 import { useWeb3React } from '@web3-react/core'
-import { Injected, Walletconnect, Walletlink } from './wallet-connect/Connectors'
+import { Walletconnect, Walletlink } from './wallet-connect/Connectors'
 
 const { TabPane } = Tabs
 const { Text } = Typography
